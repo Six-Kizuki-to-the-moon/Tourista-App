7eb97fb03b11a5e48ae4b0cf40f19236cf4946d0
uzifauzi <nurfauzi.dev@gmail.com>
2023-05-31 22:45:15 +0700

    refactor: register features, add profile fragment, change bottom navigation

---
+++ app/src/main/java/com/uppermoon/touristaapp/presentation/profile/ProfileFragment.kt
@@ -0,0 +1,45 @@
+package com.uppermoon.touristaapp.presentation.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.uppermoon.touristaapp.R
+import com.uppermoon.touristaapp.databinding.FragmentNotificationsBinding
+import com.uppermoon.touristaapp.databinding.FragmentProfileBinding
+import com.uppermoon.touristaapp.presentation.notifications.NotificationsViewModel
+
+
+class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val notificationsViewModel =
+            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textProfile
+        notificationsViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/uppermoon/touristaapp/presentation/register/RegisterActivity.kt
+++ app/src/main/java/com/uppermoon/touristaapp/presentation/register/RegisterActivity.kt
@@ -1,19 +1,33 @@
 package com.uppermoon.touristaapp.presentation.register
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
 import com.uppermoon.touristaapp.R
+import com.uppermoon.touristaapp.data.UserResult
+import com.uppermoon.touristaapp.data.preferences.UserPreferences
+import com.uppermoon.touristaapp.data.preferences.ViewModelFactory
 import com.uppermoon.touristaapp.databinding.ActivityRegisterBinding
+import com.uppermoon.touristaapp.presentation.login.LoginActivity
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var registerViewModel: RegisterViewModel
 
-    private lateinit var name: String
+    private lateinit var username: String
     private lateinit var email: String
     private lateinit var password: String
     private lateinit var confirmPassword: String
 
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "token")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,5 +36,62 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val btnRegister = binding.btnRegister
+
+        val pref = UserPreferences.getInstance(dataStore)
+        val factory = ViewModelFactory.getInstance(this, pref)
+        registerViewModel = ViewModelProvider(this, factory).get(
+            RegisterViewModel::class.java
+        )
+
+        btnRegister.setOnClickListener {
+            username = binding.etUsername.text.toString()
+            email = binding.etEmail.text.toString()
+            password = binding.etPassword.text.toString()
+            confirmPassword = binding.etConfirmPassword.text.toString()
+            when {
+                username == "" -> {
+                    binding.etUsername.error = "nama tidak boleh kosong!"
+                }
+                email == "" -> {
+                    binding.etEmail.error = "email tidak boleh kosong!"
+                }
+                password == "" -> {
+                    binding.etPassword.error = "password tidak boleh kosong!"
+                }
+                confirmPassword == "" -> {
+                    binding.etConfirmPassword.error = "password tidak boleh kosong!"
+                }
+                else -> {
+                    userRegister()
+                }
+            }
+        }
+    }
+
+    private fun userRegister() {
+        registerViewModel.postRegister(
+            username, email, password, confirmPassword
+        ).observe(this) { result ->
+            when (result) {
+                is UserResult.Success -> {
+                    showLoading(false)
+                    Toast.makeText(this, "Akun berhasil dibuat", Toast.LENGTH_SHORT).show()
+                    val intentLogin = Intent(this, LoginActivity::class.java)
+                    startActivity(intentLogin)
+                }
+                is UserResult.Loading -> {
+                    showLoading(true)
+                }
+                is UserResult.Error -> {
+                    Toast.makeText(this, "Akun gagal dibuat", Toast.LENGTH_SHORT).show()
+                    showLoading(false)
+                }
+            }
+        }
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
     }
 }
