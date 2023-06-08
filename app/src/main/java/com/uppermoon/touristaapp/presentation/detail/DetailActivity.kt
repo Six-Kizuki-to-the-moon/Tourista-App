386286752a2fe664dbbc8f5616f1ebd414608e86
uzifauzi <nurfauzi.dev@gmail.com>
2023-06-09 00:47:50 +0700

    refactor: build detail activity, card umkm, and review activity

---
+++ app/src/main/java/com/uppermoon/touristaapp/presentation/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.uppermoon.touristaapp.presentation.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.uppermoon.touristaapp.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
--- app/src/main/java/com/uppermoon/touristaapp/presentation/review/ReviewActivity.kt
+++ app/src/main/java/com/uppermoon/touristaapp/presentation/review/ReviewActivity.kt
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Layout
 import android.view.LayoutInflater
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.uppermoon.touristaapp.R
 import com.uppermoon.touristaapp.data.dummy.Review
 import com.uppermoon.touristaapp.databinding.ActivityReviewBinding
+import com.uppermoon.touristaapp.ui.adapter.ListDestinationAdapter
+import com.uppermoon.touristaapp.ui.adapter.ListReviewAdapter
 
 class ReviewActivity : AppCompatActivity() {
     private lateinit var binding: ActivityReviewBinding
@@ -20,6 +23,35 @@ class ReviewActivity : AppCompatActivity() {
         binding = ActivityReviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        rvReview = binding.rvReview
+        list.addAll(getReviewList())
+        showRecyclerList()
 
+    }
+
+    private fun showRecyclerList() {
+        rvReview.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        val listReviewAdapter = ListReviewAdapter(list)
+        rvReview.adapter = listReviewAdapter
+    }
+
+    private fun getReviewList(): ArrayList<Review> {
+        val dataUsername = resources.getStringArray(R.array.username)
+        val dataRating = resources.getStringArray(R.array.user_rating)
+        val dataPhoto = resources.getStringArray(R.array.photo_url)
+        val dataReview = resources.getStringArray(R.array.review)
+        val listReview = ArrayList<Review>()
+
+        for (i in dataUsername.indices) {
+            val review = Review(
+                dataUsername[i],
+                dataRating[i],
+                dataPhoto[i],
+                dataReview[i]
+            )
+            listReview.add(review)
+        }
+        return listReview
     }
 }
